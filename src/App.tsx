fd6e71de8d825fac04b4895af597dcbd661a7957
nicole-cab <nc482@exeter.ac.uk>
2023-08-01 02:59:59 +0100

    Single page react app to multi page (need to fix 404 error)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="about" Component={About} />
+        <Route path="skills" Component={Skills} />
+        <Route path="projects" Component={Projects} />
+        <Route path="contact" Component={Contact} />
+      </Routes>
+    </>
   );
 }
 
